d6e4c3de84102d84be636ff9849b6b7e06ebec90
unknown <antuvhai@gmail.com>
2025-12-29 20:32:06 +0600

    added email password sign up sign in validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express, { Request, Response } from "express";
 import { postRouter } from "./modules/post/post.routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from 'cors';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors({
+  origin:[process.env.APP_URL!]
+}))
 app.get("/", async (req: Request, res: Response) => {
   res.send("hello prisma blog app server");
 });
 
 app.use("/posts",postRouter)
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 
 export default app;
